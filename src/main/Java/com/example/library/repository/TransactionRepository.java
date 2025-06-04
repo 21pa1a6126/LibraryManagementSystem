ca75f4502b7de42948a41f3672ee0dc89f8b297b
21pa1a6126 <126564281+21pa1a6126@users.noreply.github.com>
2025-06-04 12:56:08 +0530

    Update TransactionRepository.java

---
+++ src/main/Java/com/example/library/repository/TransactionRepository.java
@@ -0,0 +1,11 @@
+package com.example.library.repository;Repository<User, Long> {}
+
+
+import com.example.library.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    List<Transaction> findByUserId(Long userId);
+
