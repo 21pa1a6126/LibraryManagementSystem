e714d1be9a4621d8a41a7bcf0b26c6b995180576
21pa1a6126 <126564281+21pa1a6126@users.noreply.github.com>
2025-06-04 12:56:31 +0530

    Update UserRepository.java

---
+++ src/main/Java/com/example/library/repository/UserRepository.java
@@ -0,0 +1,6 @@
+package com.example.library.repository;
+
+import com.example.library.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {}
